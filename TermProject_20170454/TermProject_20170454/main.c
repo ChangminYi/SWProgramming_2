17668f4ca10066c1c06f760d4266556509dfc76a
ChangminYi <jerry9803@naver.com>
2017-11-02 17:41:55 +0900

    Project Start

---
+++ TermProject_20170454/TermProject_20170454/main.c
@@ -0,0 +1,18 @@
+/*
+SW프로그래밍(2) Term Project
+20170454 이창민
+*/
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <process.h>
+*/
+
+void main() {
+
+
+	system("pause");
+	return;
+}
